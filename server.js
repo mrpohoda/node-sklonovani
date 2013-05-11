43fa5f268591836a73e5f161b6e680ff0759ee59
Jan Liska <mr.pohoda@gmail.com>
2013-05-11 13:05:59 +0200

    1st working version.

---
+++ server.js
@@ -0,0 +1,38 @@
+/* Main application entry file. The order of loading is important.
+ * Configuration loading and booting of controllers and custom error handlers */
+
+var express = require('express'),
+	sklonovani = require('./routes/sklonovani');
+
+var app = express();
+
+//app.use(express.logger());
+app.use(express.compress());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+//app.use(allowCrossDomain());
+app.use(app.router);
+app.use(function(err, req, res, next){
+  // logic for error handler
+});
+
+// control of favicon
+app.get('/favicon.ico', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+	res.end();
+	//console.log('favicon requested');
+	return;
+});
+
+app.get('/', function(req, res){
+	//console.log(req.ip);
+	//console.log(req.originalUrl);	// path is without arguments
+	res.send({working: 'ok'});
+});
+
+var port = process.env.PORT || 4444;
+app.listen(port);
+console.log('Listening on port ' + port);
+
+
+app.get('/sklonovani/:id', sklonovani.findOne);
